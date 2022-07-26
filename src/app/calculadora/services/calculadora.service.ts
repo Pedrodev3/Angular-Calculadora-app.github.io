dd8299f305e5a4d8d0dcf2753fb58487f06ce4d0
Pedro Victor Saraiva <pedrosaraiva@ibm.com>
2022-07-26 11:53:32 -0300

    :white_check_mark: 	Testes para operações - Teste para as operações da calculadora

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculadoraService {
+  static readonly SOMA: string = 'SOMA';
+  static readonly SUBTRACAO: string = 'SUBTRACAO';
+  static readonly DIVISAO: string = 'DIVISAO';
+  static readonly MULTIPLICACAO: string = 'MULTIPLICACAO';
 
-  constructor() { }
+  constructor() {}
+
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number; //armazena o resultado da operação
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+    
+    return resultado;
+  }
 }
