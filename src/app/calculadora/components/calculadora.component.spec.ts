4718ea17a4430b73121cfd9971166a89d204e58c
Pedro Victor Saraiva <pedrosaraiva@ibm.com>
2022-07-29 11:11:50 -0300

    :test_tube: test: Teste final para CalculadoraComponent - Relacionado a experiência do usuário e funcionamento

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { CalculadoraComponent } from './calculadora.component';
-import {CalculadoraService} from '../services';
+import { CalculadoraService } from '../services';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -9,9 +11,8 @@ describe('CalculadoraComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CalculadoraComponent],
-      providers: [CalculadoraService]
-    })
-    .compileComponents();
+      providers: [CalculadoraService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
@@ -20,5 +21,30 @@ describe('CalculadoraComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let calcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null); // Método para chamar e fazer com que um evento seja executado
+    fixture.detectChanges(); /* Como o teste é manual, você precisa executar esse método para mostrar que
+                               a operação logo acima foi detectada*/
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    calcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5'); /* Verificar se o valor que está no campo de texto do display
+    é realmente igual a 5 */
+    
   });
 });
